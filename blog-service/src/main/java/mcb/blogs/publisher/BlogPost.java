e4b0743b65750890907a795b3eb4b74264e38473
Huet Arnaud <arno.vkh@gmail.com>
2018-06-05 10:43:03 +0400

    added creator to blogpost

--- blog-service/src/main/java/mcb/blogs/publisher/BlogPost.java
+++ blog-service/src/main/java/mcb/blogs/publisher/BlogPost.java
@@ -1,5 +1,7 @@
 package mcb.blogs.publisher;
 
+import mcb.blogs.authentication.BlogUser;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,13 +16,16 @@ public class BlogPost {
 
     private String body;
 
+    private BlogUser creator;
+
     protected BlogPost() {
     }
 
     public BlogPost(String title, String body) {
         this.title = title;
         this.body = body;
-    }
+        this.creator= new BlogUser();
+}
 
     public Long getId() {
         return id;
@@ -32,5 +37,13 @@ public class BlogPost {
 
     public String getBody() {
         return body;
+    }
+
+    public BlogUser getCreator() {
+        return creator;
+    }
+
+    public void setCreator(BlogUser user) {
+        this.creator=user;
     }
 }
